228c446a5bbc46ec1aff95c6a946356265a57c77
Anthony <ajxs@panoptic.online>
2021-12-16 22:34:57 +1100

    Improve pitch EG processing documentation

--- yamaha_dx7_rom_v1.8.asm
+++ yamaha_dx7_rom_v1.8.asm
@@ -11148,7 +11148,11 @@ PORTA_PROCESS:
 ; LOCATION: 0xE616
 ;
 ; DESCRIPTION:
-; The env level goes from 0-5 to take into account the first and final levels.
+; Processes the pitch EG for all voices.
+; This subroutine loads the levels of each of the synth's 16 voices, testing
+; whether each of them is above, or below the final level for its current
+; step. Adding or subtracting the pitch EG rate's corresponding increment
+; accordingly.
 ;
 ; ==============================================================================
 
@@ -11160,85 +11164,115 @@ PITCH_EG_PROCESS:
     LDAB    #16
     STAB    <M_PITCH_EG_VOICE_INDEX
 
+; Check whether the current pitch EG step is '3'. This indicates that the
+; pitch EG for this voice is has reached its 'sustain' value.
+; When a voice is removed, the pitch EG step for that voice is set to '4',
+; which places the voice's pitch EG into its 'release' phase.
+; This check ensures that a voice that's in its 'note on' phase does not
+; process the pitch EG past the sustain phase.
+
 .LOOP_START:
     LDAA    0,x
     CMPA    #3
     BNE     .IS_ENV_STEP_5?
     JMP     .INCREMENT_POINTERS
 
+; As noted above, when a voice is removed its pitch EG step is set to '4'.
+; This places the voice in its 'release' phase.
+; This check ensures that if the voice has reached step '5' (the end), it
+; will not be processed further.
+
 .IS_ENV_STEP_5?:
     CMPA    #5
-    BNE     .EG_STEP_NOT_5
+    BNE     .PROCESS_EG_STAGE
     JMP     .INCREMENT_POINTERS
 
-.EG_STEP_NOT_5:
+; Load the pitch EG values, clamping the index at 3.
+
+.PROCESS_EG_STAGE:
     LDX     #M_PATCH_PITCH_EG_VALUES
-    CMPA    #3                                  ; Clamp A at 3.
-    BCS     .LOAD_PITCH_EG_RATE                 ; If A < 3, branch.
+    CMPA    #3
+    BCS     .LOAD_PITCH_EG_RATE
     LDAA    #3
 
-; Load PITCH_EG_RATE[B].
+; The following section loads the current patch's parsed pitch EG rate,
+; using ACCB as an index into the 4 entry array.
+; This value is used to compute the 'Pitch EG increment' value, which is
+; the delta for each iteration of processing the pitch EG.
+; It then loads the current patch's parsed pitch EG level, using ACCB as an
+; index into this array, and uses this to compute the 'next' EG level, which
+; the current level is compared against.
 
 .LOAD_PITCH_EG_RATE:
     TAB
     ABX
-    LDAB    0,x
+    LDAB    0,x                                 ; Load PITCH_EG_RATE[B].
     CLRA
     STD     <M_PITCH_EG_INCREMENT
 
-; Load PITCH_EG_LEVEL[B].
-
-.LOAD_PITCH_EG_LEVEL:
+.LOAD_PITCH_EG_LEVEL:                           ; Load PITCH_EG_LEVEL[B].
     LDAA    4,x
     CLRB
     LSRD
     STD     <M_PITCH_EG_NEXT_LVL
 
-.COMPARE_TO_CURRENT?:
+; Compare the current pitch EG level against the 'next' level.
+; If it is equal, the incrementing/decrementing step is skipped.
+
+.COMPARE_TO_CURRENT:
     LDX     <M_PITCH_EG_VOICE_LEVELS_PTR
     LDX     0,x
     CPX     <M_PITCH_EG_NEXT_LVL
-    BEQ     .STORE_NEXT_LEVEL
+    BEQ     .EG_STEP_FINISHED
+
+; Test whether the current level is above or below the final, target level.
 
 .IS_LEVEL_HIGHER?:
     BCS     .PITCH_EG_LEVEL_HIGHER
+
+
+; Subtract the increment value from the current level. If the value goes
+; below 0, this means that the current step is finished.
 
 .PITCH_EG_LEVEL_LOWER:
     LDX     <M_PITCH_EG_VOICE_LEVELS_PTR
     LDD     0,x
     SUBD    <M_PITCH_EG_INCREMENT
-
-; If the value goes below 0, this step is finished.
-    BMI     .STORE_NEXT_LEVEL
+    BMI     .EG_STEP_FINISHED
 
 .IS_PITCH_EG_LEVEL_LOWER_FINISHED?:
     CMPA    <M_PITCH_EG_NEXT_LVL
-    BHI     .STORE_CURRENT_PITCH_EG_LEVEL
-    BRA     .STORE_NEXT_LEVEL
+    BHI     .EG_STEP_NOT_FINISHED
+    BRA     .EG_STEP_FINISHED
+
+; If the target pitch EG level is higher than the current level, add the
+; pitch EG increment to the current level, and compare.
 
 .PITCH_EG_LEVEL_HIGHER:
     LDX     <M_PITCH_EG_VOICE_LEVELS_PTR
     LDD     0,x
-
-; Add the pitch EG increment to the current level.
     ADDD    <M_PITCH_EG_INCREMENT
     CMPA    <M_PITCH_EG_NEXT_LVL
 
 ; If the value is still higher than the target, branch.
-; Otherwise we know we're at the target level.
-    BCS     .STORE_CURRENT_PITCH_EG_LEVEL
+; Otherwise we know we're at the final level for this step.
+    BCS     .EG_STEP_NOT_FINISHED
 
-.STORE_NEXT_LEVEL:
+; If this EG step has finished, store the 'next' pitch EG level in the
+; 'current' level. This has the purpose of allowing the value to overflow,
+; or underflow during the increment stage without causing any ill-effects.
+
+.EG_STEP_FINISHED:
     LDD     <M_PITCH_EG_NEXT_LVL
     LDX     <M_PITCH_EG_VOICE_LEVELS_PTR
     STD     0,x
 
-.INCREMENT_ENV_STEP?:
+; Increment the EG step. If the step is 6 after incrementing, set to 0.
+
+.INCREMENT_EG_STEP:
     LDX     <M_PITCH_EG_VOICE_STEP_PTR
     LDAA    0,x
     INCA
-
-; If after incrementing the env step is 6, set to 0.
     CMPA    #6
     BNE     .STORE_ENV_STEP
     CLRA
@@ -11247,9 +11281,13 @@ PITCH_EG_PROCESS:
     STAA    0,x
     BRA     .INCREMENT_POINTERS
 
-.STORE_CURRENT_PITCH_EG_LEVEL:
+; If the current step is not finished, save the current level.
+
+.EG_STEP_NOT_FINISHED:
     LDX     <M_PITCH_EG_VOICE_LEVELS_PTR
     STD     0,x
+
+; Increment the pointers to point to the next voice.
 
 .INCREMENT_POINTERS:
     LDX     <M_PITCH_EG_VOICE_LEVELS_PTR
@@ -11257,7 +11295,7 @@ PITCH_EG_PROCESS:
     INX
     STX     <M_PITCH_EG_VOICE_LEVELS_PTR
 
-.INCREMENT_ENV_STEP_PTR:
+.INCREMENT_EG_STEP_PTR:
     LDX     <M_PITCH_EG_VOICE_STEP_PTR
     INX
     STX     <M_PITCH_EG_VOICE_STEP_PTR
